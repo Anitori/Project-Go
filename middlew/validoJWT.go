a76e321b4865bdfecf226e06d9339c88210394dd
Emanuel Rodriguez Anitori <88413740+Anitori@users.noreply.github.com>
2022-08-19 19:29:18 -0300

    cambio directorio routes, por routers

--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -2,7 +2,7 @@ package middlew
 
 import (
 	"net/http"
-	"github.com/Anitori/Project-Go/routes"
+	"github.com/Anitori/Project-Go/routers"
 )
 
 //Permite validar el JWT que nos viene en la petición
@@ -10,6 +10,11 @@ import (
 func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {  //Recordar que cuando es un middleware, recibe y devuelve lo mismo
 	return func(w http.ResponseWriter, r *http.Request){ //Va a llamar a una rutina creada en routers que va a devolver 4 parámetros (acá solo queremos saber si hay o no error en la ejecución de dicha rutina)
 
-	_, _, _, err := routes.ProcesoToken(r.Header.Get("Authorization"))
+	_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
+	if err != nil {
+		http.Error(w, "Error en el Token ! "+err.Error(), http.StatusBadRequest)
+	}
+
+	next.ServeHTTP(w, r)
 	}
 } 
